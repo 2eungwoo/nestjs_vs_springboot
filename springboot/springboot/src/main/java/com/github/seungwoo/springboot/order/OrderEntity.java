431c46c2deff3b2d15dfadadd2f6d6d2d7760d20
swlee <swo98@naver.com>
2025-12-13 21:32:13 +0900

    added: order entity 추가

---
+++ springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/OrderEntity.java
@@ -0,0 +1,43 @@
+package com.github.seungwoo.springboot.order;
+
+import com.github.seungwoo.springboot.common.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false)
+    private Long price;
+
+    private OrderEntity(String productName, Integer quantity, Long price) {
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public static OrderEntity of(String productName, Integer quantity, Long price) {
+        return new OrderEntity(productName, quantity, price);
+    }
+}
