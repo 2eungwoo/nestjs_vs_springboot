4238c7b3b4465a9a2ab15234369b002fb9728803
swlee <swo98@naver.com>
2025-12-13 21:42:15 +0900

    added: order entity 추가, nestjs관례에 맞는 스타일로 작성

---
+++ nestjs/src/orders/order.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseTimeEntity } from '../common/base-time.entity';
+
+@Entity({ name: 'orders' })
+export class OrderEntity extends BaseTimeEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id!: number;
+
+  @Column({ name: 'product_name', nullable: false })
+  productName!: string;
+
+  @Column({ type: 'integer', nullable: false })
+  quantity!: number;
+
+  @Column({ type: 'bigint', nullable: false })
+  price!: number;
+
+  constructor(partial?: Partial<OrderEntity>) {
+    super();
+    if (partial) {
+      Object.assign(this, partial);
+    }
+  }
+  // nestjs 관례에 맞게 springboot랑 차이 둠
+  // entity 인스턴스는 생성자보다 orm에 의존함 (repository.create())
+}
