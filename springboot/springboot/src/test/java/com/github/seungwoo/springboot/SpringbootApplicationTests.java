e3f3f1c624c146dc86af51b12f6ddc74c9f9516c
swlee <swo98@naver.com>
2025-12-13 20:53:36 +0900

    init: nestjs + springboot init

---
+++ springboot/springboot/src/test/java/com/github/seungwoo/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.seungwoo.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
