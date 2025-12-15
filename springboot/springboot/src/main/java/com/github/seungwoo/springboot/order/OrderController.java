3ba305a7539c7b3668268dcc3fdd1c8ece2bbe62
swlee <swo98@naver.com>
2025-12-15 19:57:39 +0900

    fixed: paging find all이랑 non paging을 엔드포인트 v1,v2로 구분

--- springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/OrderController.java
+++ springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/OrderController.java
@@ -4,6 +4,9 @@ import com.github.seungwoo.springboot.order.dto.CreateOrderRequest;
 import com.github.seungwoo.springboot.order.dto.OrderResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,5 +21,15 @@ public class OrderController {
     @PostMapping("/orders")
     public List<OrderResponse> createOrders(@RequestBody List<CreateOrderRequest> requests) {
         return orderService.createOrders(requests);
+    }
+
+    @GetMapping("/orders/v1")
+    public List<OrderResponse> getOrders() {
+        return orderService.findAllOrders();
+    }
+
+    @GetMapping("/orders/v2")
+    public Page<OrderResponse> getOrders(Pageable pageable) {
+        return orderService.findAllPagingOrders(pageable);
     }
 }
--- springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/OrderService.java
+++ springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/OrderService.java
@@ -5,6 +5,8 @@ import com.github.seungwoo.springboot.order.dto.OrderResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,5 +28,18 @@ public class OrderService {
 
         List<OrderEntity> saved = orderRepository.saveAll(entities);
         return saved.stream().map(OrderResponse::from).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<OrderResponse> findAllOrders() {
+        return orderRepository.findAll().stream()
+            .map(OrderResponse::from)
+            .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderResponse> findAllPagingOrders(Pageable pageable) {
+        return orderRepository.findAll(pageable)
+            .map(OrderResponse::from);
     }
 }
