f0c2cf554727456f29f0b8bc838f4d2ffb0f1b26
swlee <swo98@naver.com>
2025-12-15 20:00:35 +0900

    added: paging/nonpaging findall 엔드포인트 추가

--- nestjs/src/orders/order.controller.ts
+++ nestjs/src/orders/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderResponseDto } from './dto/order-response.dto';
 import { OrderService } from './order.service';
@@ -10,5 +10,22 @@ export class OrderController {
   @Post('/orders')
   async createOrders(@Body() payloads: CreateOrderDto[]): Promise<OrderResponseDto[]> {
     return this.orderService.createOrders(payloads);
+  }
+
+  @Get('/orders/v1')
+  async getOrders(): Promise<OrderResponseDto[]> {
+    return this.orderService.findAllOrders();
+  }
+
+  @Get('/orders/v2')
+  async getOrdersPaging(@Query('skip') skip?: number,
+                        @Query('take') take?: number
+  ): Promise<{ data: OrderResponseDto[]; total: number }> {
+    const options = {
+      skip: skip ?? 0,
+      take: take ?? 10,
+      order: { id: 'ASC' } as const,
+    };
+    return this.orderService.findAllPagingOrders(options);
   }
 }
--- nestjs/src/orders/order.service.ts
+++ nestjs/src/orders/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderResponseDto } from './dto/order-response.dto';
 import { OrderEntity } from './order.entity';
@@ -16,5 +16,21 @@ export class OrderService {
     const entities = this.orderRepository.create(payloads);
     const saved = await this.orderRepository.save(entities);
     return saved.map((entity) => new OrderResponseDto(entity));
+  }
+
+  async findAllOrders(): Promise<OrderResponseDto[]> {
+    const orders = await this.orderRepository.find();
+    return orders.map((entity) => new OrderResponseDto(entity));
+  }
+
+  async findAllPagingOrders(options: FindManyOptions<OrderEntity>): Promise<{
+    data: OrderResponseDto[];
+    total: number;
+  }> {
+    const [orders, total] = await this.orderRepository.findAndCount(options);
+    return {
+      data: orders.map((entity) => new OrderResponseDto(entity)),
+      total,
+    };
   }
 }
