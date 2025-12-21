15a523fbb84f288ebeaf2943613d6ddce3173a7d
swlee <swo98@naver.com>
2025-12-21 18:21:46 +0900

    added: fastapi에 만든 /hash 연동할 데이터받을 properties클래스 추가

---
+++ springboot/springboot/src/main/java/com/github/seungwoo/springboot/order/HashApiProperties.java
@@ -0,0 +1,16 @@
+package com.github.seungwoo.springboot.order;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "hash.api")
+public class HashApiProperties {
+
+    // URL : /hash (fastapi)
+    private final String url = "http://127.0.0.1:7000/hash";
+
+    public String getUrl() {
+        return url;
+    }
+}
